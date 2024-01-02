c4fef9580b7d07cf3ebc385afbe4c8dde6c5cced
sleefd <sleefd@gmail.com>
2024-01-02 16:48:41 +0800

    using string class directly

--- lang-bridge/src/main/scala/me/yceel/bridge/JTypes.scala
+++ lang-bridge/src/main/scala/me/yceel/bridge/JTypes.scala
@@ -16,7 +16,7 @@ object JTypes {
       case GoType.GoUInt64  => TypeName.LONG
       case GoType.GoFloat32 => TypeName.FLOAT
       case GoType.GoChar    => TypeName.CHAR
-      case GoType.GoString  => ClassName.bestGuess("String")
+      case GoType.GoString  => ClassName.get(classOf[String])
       case GoType.GoBool    => TypeName.BOOLEAN
       case GoType.GoArray(element) =>
         val eleType = ctx.getOrElseUpdate(element, from(element, ctx))
